befc406501a83c43e69d53cbe0172206f865c004
Tom Rosenblatt <rosenblatttom@gmail.com>
2018-01-05 20:32:26 -0800

    working towards offline queue

--- client/ourLibrary-client.js
+++ client/ourLibrary-client.js
@@ -1,5 +1,6 @@
 import React, { Component, cloneElement } from 'react';
 import io from 'socket.io-client';
+import localforage from 'localforage';
 
 class Controller extends Component {
   constructor(props) {
@@ -19,15 +20,16 @@ class Controller extends Component {
 }
 
 let store,
-currentCallback;
-const socket = io('https://boiling-cove-32080.herokuapp.com/');
+  currentCallback;
+// const socket = io('https://boiling-cove-32080.herokuapp.com/');
+const socket = io('https://localhost:3000');
 
 export const Wrapper = () => {
   store = new Controller();
   return store;
 };
 
-export const get = (key) => store.state[key];
+export const get = key => store.state[key];
 
 export const set = (key, value, runQueries = true, callback) => {
   if (callback) {
@@ -37,56 +39,54 @@ export const set = (key, value, runQueries = true, callback) => {
     store.addToStore(key, value);
   }
   socket.emit('set', { key, value, runQueries });
-  if (localStorage.getItem('queue')) {
-    const queue = localStorage.getItem('queue');
-    queue.push({
- set, key, value, runQueries, callback
-});
-    localStorage.setItem('queue', queue);
-  } else {
-    localStorage.setItem('queue', [{
- set, key, value, runQueries, callback
-}]);
-  }
+  localforage.getItem('queue', (err, queue) => {
+    if (queue) {
+      queue.push({set, key, value, runQueries, callback});
+      localforage.setItem('queue', queue);
+    } else {
+      localforage.setItem('queue', [{set, key, value, runQueries, callback}]);
+    }
+  });
 };
 
-export const query = (key, callback, values) => {
-  socket.emit('query', { key, values });
-  currentCallback = callback; // ****wonder if we need to track this query with a unique id from client
-  // to server and back to server so that the correct currentCallback is called
-  if (localStorage.getItem('queue')) {
-    const queue = localStorage.getItem('queue');
-    queue.push({
- query, key, value, runQueries, callback
-});
-    localStorage.setItem('queue', queue);
-  } else {
-    localStorage.setItem('queue', [{
- query, key, value, runQueries, callback
-}]);
-  }
+export const query = (key, callback, value) => {
+  socket.emit('query', { key, value });
+  currentCallback = callback;
+  localforage.getItem('queue', (err, queue) => {
+    if (queue) {
+      queue.push({query, key, value, callback});
+      localforage.setItem('queue', queue);
+    } else {
+      localforage.setItem('queue', [{query, key, value, callback}]);
+    }
+  });
 };
 
 socket.on('local', () => {
   console.log('back connecteddddd');
-  const list = localStorage.getItem('queue');
-  list.forEach((x) => {
-    if (x.set) {
-      socket.emit('set', { key: x.key, value: x.value, runQueries: x.runQueries });
-    } else if (x.query) {
-      socket.emit('query', { key: x.key, value: x.value });
-    }
-  });
+  if (localforage.getItem('queue') !== null) {
+    const list = localforage.getItem('queue');
+    console.log('list ', list);
+    list.forEach((x) => {
+      if (x.set) {
+        socket.emit('set', { key: x.key, value: x.value, runQueries: x.runQueries });
+      } else if (x.query) {
+        socket.emit('query', { key: x.key, value: x.value });
+      }
+    });
+  }
 });
 
 socket.on('response', (data) => {
   set(data.key, data.response, false);
-  const queue = localStorage.getItem('queue');
-  localStorage.setItem(queue, queue.shift());
+  const queue = localforage.getItem('queue');
+  queue.shift();
+  localforage.setItem('queue', queue);
 });
 
 socket.on('queryResponse', (data) => {
   currentCallback(data);
-  const queue = localStorage.getItem('queue');
-  localStorage.setItem(queue, queue.shift());
+  const queue = localforage.getItem('queue');
+  queue.shift();
+  localforage.setItem('queue', queue);
 });
